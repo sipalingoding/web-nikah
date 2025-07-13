a2ce2be31bcfa57f53dc388655bf8226a61e84e2
sipalingoding <arsenalsalmanfpl@gmail.com>
2025-07-13 20:07:06 +0700

    FIXING THUMBNAIK

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -18,7 +18,7 @@ export const metadata = {
     type: "website",
     images: [
       {
-        url: "https://salman-karima.com/images/tumbnail.png",
+        url: "https://salman-karima.com/images/thumbnail.png",
         width: 1200,
         height: 630,
         alt: "Salman & Karima",
